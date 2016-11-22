86df6f358277f3b441d693f760aa565abf604fa7
Tom Wilson <tom@jackhq.com>
2016-11-22 09:40:06 -0500

    locations select working

--- web/src/pages/efforts/form.js
+++ web/src/pages/efforts/form.js
@@ -1,14 +1,29 @@
 const React = require('react')
 const {Link, Redirect} = require('react-router')
 
+const xhr = require('xhr')
+
 const EffortForm = React.createClass({
   getInitialState: function() {
     return {
       success: false,
-      effort: {}
+      effort: {},
+      locations: [{id: "-1", name: "Choose"}]
     };
   },
   componentDidMount() {
+    xhr.get(process.env.REACT_APP_API + '/locations',
+    {
+      json: true,
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('id_token')}`
+      }
+    }
+    , (e,r,b) => {
+      if (e) return console.log(e.message)
+      this.setState({locations: [].concat(this.state.locations, b) })
+    })
+
     if (this.props.params.id) {
       this.props.get(this.props.params.id, (err, effort) => {
         if (err) return console.log(err.message)
@@ -45,6 +60,8 @@ const EffortForm = React.createClass({
     const labelStyle = {display: 'block'}
     const effort = this.state.effort || {}
 
+    const option = location => <option value={location.id}>{location.name}</option>
+
     return (
       <div>
         { this.state.success ? <Redirect to="/efforts" /> : null }
@@ -64,7 +81,11 @@ const EffortForm = React.createClass({
           </div>
           <div>
             <label style={labelStyle}>Location</label>
-            <select></select>
+            <select
+              value={this.state.effort.location_id}
+              onChange={this.handleChange('location_id')}>
+              {this.state.locations.map(option)}
+            </select>
           </div>
           <div>
             <button>Submit</button>
